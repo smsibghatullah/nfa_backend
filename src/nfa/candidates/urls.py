391eb5143362d3221dee751a7de305dba6155c06
Muhammad Ehsan Shaikh <ehsanshaikh77@gmail.com>
2025-08-11 02:14:53 +0200

    feat(contact, admin): add contact-us API with unique file uploads and submitted forms admin view

--- src/nfa/candidates/urls.py
+++ src/nfa/candidates/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_schedule
+from .views import upload_schedule, contact_us
 
 urlpatterns = [
     path('upload-schedule/', upload_schedule, name='upload-schedule'),
+    path('contact-us/', contact_us, name='contact-us'),
 ]
