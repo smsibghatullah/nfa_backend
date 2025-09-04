3d35db3383f1259942391b06331e2d1103d025ab
Muhammad Ehsan Shaikh <ehsanshaikh77@gmail.com>
2025-09-04 02:27:23 +0200

    feat: 2fa and some other changes in authentication, 2fa is just added, we will see it in future

--- src/nfa/authentication/admin.py
+++ src/nfa/authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    list_display = ("email", "username", "is_active", "is_staff", "date_joined")
+    ordering = ("email",)
+    search_fields = ("email", "username", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "username")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
--- src/nfa/authentication/serializers.py
+++ src/nfa/authentication/serializers.py
@@ -1,54 +1,51 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth import authenticate
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]  
+        validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)  
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password", "password2", "first_name", "last_name")
+        fields = ("email", "password", "password2", "username", "first_name", "last_name")
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if attrs["password"] != attrs["password2"]:
             raise serializers.ValidationError({"password": "Passwords do not match."})
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', ''),
-            is_active=True
-        )
+        validated_data.pop("password2", None)
+        password = validated_data.pop("password")
+        user = User.objects.create_user(password=password, **validated_data)
         return user
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
+    otp = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     def validate(self, data):
-        email = data['email']
-        password = data['password']
-        try:
-            user_obj = User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError("Invalid credentials.")
-        user = authenticate(username=user_obj.username, password=password)
+        email = data.get("email")
+        password = data.get("password")
+        otp = data.get("otp")
+
+        user = authenticate(self.context.get("request"), email=email, password=password)
         if not user or not user.is_active:
-            raise serializers.ValidationError("Invalid credentials or inactive user.")
-        data['user'] = user
+            raise serializers.ValidationError({"detail": "Invalid email or password."})
+
+        data["user"] = user
+        data["otp"] = otp
         return data
-    
+
 class LogoutSerializer(serializers.Serializer):
     refresh = serializers.CharField()
 
@@ -63,3 +60,12 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
     uid = serializers.CharField(required=True)
     token = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True, validators=[validate_password])
+
+class TOTPSetupSerializer(serializers.Serializer):
+    pass
+
+class TOTPVerifySetupSerializer(serializers.Serializer):
+    otp = serializers.CharField(required=True)
+
+class TOTPDisableSerializer(serializers.Serializer):
+    confirm = serializers.BooleanField(required=True)
