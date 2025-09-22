3c3b119ca0515c67bbb7734a568aa06167ee5001
Muhammad Ehsan Shaikh <ehsanshaikh77@gmail.com>
2025-09-22 04:32:48 +0200

    fix: login register apis updated with cnic

--- src/nfa/authentication/serializers.py
+++ src/nfa/authentication/serializers.py
@@ -7,7 +7,13 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
-        required=True,
+        required=False,
+        allow_blank=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    cnic = serializers.CharField(
+        required=False,
+        allow_blank=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -15,9 +21,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("email", "password", "password2", "username", "first_name", "last_name")
+        fields = ("email", "cnic", "password", "password2", "first_name", "last_name")
 
     def validate(self, attrs):
+        if not attrs.get("email") and not attrs.get("cnic"):
+            raise serializers.ValidationError("Either email or CNIC is required.")
+
         if attrs["password"] != attrs["password2"]:
             raise serializers.ValidationError({"password": "Passwords do not match."})
         return attrs
@@ -25,22 +34,26 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password2", None)
         password = validated_data.pop("password")
-        user = User.objects.create_user(password=password, **validated_data)
-        return user
+        return User.objects.create_user(password=password, **validated_data)
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=False, allow_blank=True)
+    cnic = serializers.CharField(required=False, allow_blank=True)
     password = serializers.CharField(write_only=True, required=True)
     otp = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     def validate(self, data):
-        email = data.get("email")
+        email = data.get("email") or None
+        cnic = data.get("cnic") or None
         password = data.get("password")
         otp = data.get("otp")
 
-        user = authenticate(self.context.get("request"), email=email, password=password)
+        if not email and not cnic:
+            raise serializers.ValidationError({"detail": "Email or CNIC is required."})
+
+        user = authenticate(self.context.get("request"), email=email, cnic=cnic, password=password)
         if not user or not user.is_active:
-            raise serializers.ValidationError({"detail": "Invalid email or password."})
+            raise serializers.ValidationError({"detail": "Invalid credentials."})
 
         data["user"] = user
         data["otp"] = otp
